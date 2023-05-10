53e71ee421e3176272a8ab1a9748f119bf2b290d
Azim <azimreact@gmail.com>
2023-05-11 00:05:34 +0600

    replaced if else to just if constuctions

--- tzFindNums.js
+++ tzFindNums.js
@@ -6,6 +6,7 @@ const nums3 = [1, 3, 5];
 const nums4 = [1, 2, 4, 5, 6, 7, 9, 10];
 const nums5 = [1, 4, 5, 6, 8, 7, 9, 10];
 const nums6 = [1, 2, 3, 4, 5, 6, 7, 10];
+const nums7 = [1, 3, 4, 5, 6, 7, 8, 10];
 const nums0 = [1, 3]; // если пропущен один элемент, отработает и для [2]
 
 const findNums = (nums) => {
@@ -18,19 +19,12 @@ const findNums = (nums) => {
     while (start <= end && result.length < 2) {
         middle = Math.floor((start + end) / 2);
 
-        if (nums[middle] - nums[middle - 1] === 2) {
-            result.push(nums[middle] - 1);
-        } else if (nums[middle] - nums[middle - 1] === 3) {
-            result.push(nums[middle] - 2, nums[middle] - 1);
-        };
+        if (nums[middle] - nums[middle - 1] === 2) result.push(nums[middle] - 1);
+        if (nums[middle] - nums[middle - 1] === 3) result.push(nums[middle] - 2, nums[middle] - 1);
 
-        if (nums[middle] - middle === 1) {
-            start = middle + 1;
-        } else if (nums[middle] - middle === 2) {
-            start = middle + 1;
-        } else if (nums[middle] - middle === 3) {
-            end = middle - 1;
-        };
+        if (nums[middle] - middle === 1) start = middle + 1;
+        if (nums[middle] - middle === 2) start = middle + 1;
+        if (nums[middle] - middle === 3) end = middle - 1;
     };
 
     start = 0;
@@ -40,13 +34,9 @@ const findNums = (nums) => {
 
         if (nums[middle] - nums[middle - 1] === 2) result.push(nums[middle] - 1);
 
-        if (nums[middle] - middle === 1) {
-            start = middle + 1;
-        } else if (nums[middle] - middle === 2) {
-            end = middle - 1;
-        } else if (nums[middle] - middle === 3) {
-            end = middle - 1;
-        };
+        if (nums[middle] - middle === 1) start = middle + 1;
+        if (nums[middle] - middle === 2) end = middle - 1;
+        if (nums[middle] - middle === 3) end = middle - 1;
     };
 
     return Array.from(new Set(result));
@@ -58,6 +48,7 @@ const result3 = findNums(nums3);
 const result4 = findNums(nums4);
 const result5 = findNums(nums5);
 const result6 = findNums(nums6);
+const result7 = findNums(nums7);
 const result0 = findNums(nums0);
 
 console.log('1', result1);
@@ -66,6 +57,7 @@ console.log('3', result3);
 console.log('4', result4);
 console.log('5', result5);
 console.log('6', result6);
+console.log('6', result7);
 console.log('0', result0);
 
 // Task #2 :
